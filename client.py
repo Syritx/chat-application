d4efdca54dfbeade2ec6429f24743eace141c4dc
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-09-02 10:29:48 -0400

    Create client.py

---
+++ client.py
@@ -0,0 +1,37 @@
+import socket
+import signal
+
+# connecting client
+ip = socket.gethostbyname(socket.gethostname())
+port = 5050
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((ip,port))
+
+def handler(signum, frame):
+    client.send("DISCONNECTED_FROM_CHAT_APP")
+
+signal.signal(signal.SIGHUP, handler)
+
+def send_message(message):
+    
+    # sending messages to the server
+    msg = message.encode("utf-8")
+    msg_len = len(msg)
+
+    send_len = str(msg_len).encode("utf-8")
+    send_len += b' ' * (1024 - len(send_len))
+
+    client.send(send_len)
+    client.send(msg)
+
+    print(client.recv(2048).decode("utf-8")) # receiving messages from the server
+    get_input()
+
+
+# getting input
+def get_input():
+    msg = input()
+    send_message(msg)
+
+get_input()
